552568df4de57cd7f88dd0461bdeb2438e794118
Jackson Campolattaro <jackcamp@vt.edu>
2020-07-03 19:45:44 -0400

    Add CGAL typedefs

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,17 @@
 
 #include <Magnum/Platform/GLContext.h>
 
+#define CGAL_TRACE_STREAM std::cerr
+#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
+#include <CGAL/Point_set_3.h>
+
 #include "Viewer.h"
 
 using namespace Magnum;
+
+typedef CGAL::Exact_predicates_inexact_constructions_kernel Kernel;
+typedef Kernel::Point_3 Point;
+typedef CGAL::Point_set_3<Point> Point_set;
 
 int main(int argc, char** argv) {
 
